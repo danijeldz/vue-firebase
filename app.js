4b4048440f5fa7ddb398dd4a8523a1fb85ac3ac4
danijeldz <danijeldzankic@hotmail.com>
2019-03-16 22:23:14 +0100

    event handling

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ new Vue({
   el: "#app",
   data: {
     title: "Becoming a Vue Ninja",
-    name: "Ryu",
-    url: "https://www.youtube.com",
-    classes: ["one", "two"]
+    wage: 10,
+    coords: {
+      x: 0,
+      y: 0
+    }
   },
   methods: {
-    greet(time) {
-      return `Hello and good ${time}, ${this.name}.`;
+    changeWage(amount) {
+      this.wage += amount;
+    },
+    logEvent(e) {
+      console.log(e);
+    },
+    logCoords(e) {
+      this.coords.x = e.offsetX;
+      this.coords.y = e.offsetY;
     }
   }
 });
