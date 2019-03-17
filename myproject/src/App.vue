7230e89370848de4aad4a518de76636e1ba0f088
danijeldz <danijeldzankic@hotmail.com>
2019-03-17 10:05:25 +0100

    components

---
+++ myproject/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "My first Vue app, wooooo :)"
+    };
+  }
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  font-weight: normal;
+  color: #444;
+}
+</style>
