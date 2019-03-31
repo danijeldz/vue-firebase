7ce87f67062cee95b2a9d0c48cc21a97e08abf92
danijeldz <danijeldzankic@hotmail.com>
2019-03-31 19:19:21 +0200

    all together

---
+++ routing/src/components/Contact.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Hello!</h2>
+    <ul>
+      <li>
+        <span>{{name}}</span>
+      </li>
+      <li v-if="platform == true">
+        <a v-bind:href="'tel:'+mobile">{{mobile}}</a>
+      </li>
+      <li>
+        <a v-bind:href="'mailto:'+email">{{email}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Contact",
+  data() {
+    return {
+      email: "danijeldzankic@hotmail.com",
+      mobile: "+4915221042427",
+      name: "Danijel Dzankic",
+      platform: this.$route.params.mobile
+    };
+  },
+  methods: {
+    updatePlatform() {
+      this.platform = this.$route.params.mobile;
+    }
+  },
+  watch: {
+    $route: "updatePlatform"
+  }
+};
+</script>
+
+<style>
+</style>
+
---
+++ routing/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import About from "@/components/About";
+import ViewProfile from "@/components/ViewProfile";
+import Contact from "@/components/Contact";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: About
+    },
+    {
+      path: "/profile/:user_id",
+      name: "ViewProfile",
+      component: ViewProfile
+    },
+    {
+      path: "/contact/:mobile",
+      name: "Contact",
+      component: Contact
+    }
+  ]
+});
