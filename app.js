6acae1380a2c7d5b20b7199dba2e99ce668d1756
danijeldz <danijeldzankic@hotmail.com>
2020-03-04 11:38:11 +0100

    created v-on event handling

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ new Vue({
   el: "#app",
   data: {
     title: "Becoming a Vue Ninja",
-    name: "Ryu",
-    url: "http://www.youtube.com",
-    classes: ["one", "two"]
+    name: "Ryu"
   },
   methods: {
     greet(time) {
       return `Hello there and good ${time}, ${this.name}!`;
+    },
+    updateName(e) {
+      //console.log(e.target.value);
+      this.name = e.target.value;
     }
   }
 });
