83ab02fc6841fe60e2c62022e454463e347bf5ed
danijeldz <danijeldzankic@hotmail.com>
2020-03-04 10:14:25 +0100

    git events created based on simple methods and click and dblclick events

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
 new Vue({
   el: "#app",
   data: {
-    title: "Becoming a Vue Ninja",
-    items: ["crystal", "mashroom", "banana", "star", "Green Shells"],
-    ninjas: [
-      {
-        name: "Crystal",
-        age: 15,
-        belt: "red"
-      },
-      {
-        name: "Ryu",
-        age: 19,
-        belt: "blue"
-      },
-      {
-        name: "Yoshi",
-        age: 27,
-        belt: "black"
-      }
-    ]
+    title: "How deep is your love?",
+    wage: 10
   },
   methods: {
-    updateName(e) {
-      // console.log(e.target.value);
-      this.name = e.target.value;
-    },
-    logMe(message) {
-      console.log(message);
-    },
-    toggleName() {
-      this.showName = !this.showName;
-    },
-    toggleAge() {
-      this.showAge = !this.showAge;
+    changeWage(amount) {
+      this.wage += amount;
     }
   }
 });
