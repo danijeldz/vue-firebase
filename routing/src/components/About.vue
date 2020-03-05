c560c47324335ff8c6a25a565334436078b97ebb
danijeldz <danijeldzankic@hotmail.com>
2020-03-05 15:45:50 +0100

    created basic routs for navbar

---
+++ routing/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About me</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
---
+++ routing/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link :to="{ name: 'About' }">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+}
+ul > li {
+  display: inline-block;
+}
+a {
+  color: #42b983;
+}
+</style>
