b87764531ef792aef072ac409e03b593c18ab6b8
danijeldz <danijeldzankic@hotmail.com>
2020-03-05 11:57:08 +0100

    used lifecycle hooks

--- myproject/src/App.vue
+++ myproject/src/App.vue
@@ -2,42 +2,26 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <AllFriends :friends="friends" @delete="deleteFriend" />
-    <OnlineFriends :friends="friends" />
+    <Blogs />
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar";
-import AllFriends from "./AllFriends";
-import OnlineFriends from "./OnlineFriends";
+import Blogs from "./Blogs";
 
 export default {
   name: "app",
   components: {
     Navbar,
-    AllFriends,
-    OnlineFriends
+    Blogs
   },
   data() {
     return {
-      title: "Starting vue template",
-      friends: [
-        { name: "Mario", online: true },
-        { name: "Toad", online: false },
-        { name: "Luigi", online: true },
-        { name: "Bowser", online: false }
-      ]
+      title: "Starting vue template"
     };
   },
-  methods: {
-    deleteFriend(payload) {
-      console.log(payload);
-      this.friends = this.friends.filter(friend => {
-        return friend.name !== payload.name;
-      });
-    }
-  }
+  methods: {}
 };
 </script>
 
---
+++ myproject/src/Blogs.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="blogs">
+    <h2>{{ blogsTitle }}</h2>
+    <button v-on:click="changeTitle">Change title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blogs",
+  data() {
+    return {
+      blogsTitle: "Blogs"
+    };
+  },
+  beforeCreate() {
+    alert("beforeCreate hook");
+  },
+  created() {
+    alert("created hook");
+  },
+  beforeUpdate() {
+    alert("beforeUpdate hook");
+  },
+  updated() {
+    alert("updated hook");
+  },
+  methods: {
+    changeTitle() {
+      this.blogsTitle = "blog is on!";
+    }
+  }
+};
+</script>
+
+<style></style>
