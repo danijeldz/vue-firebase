fc3c4f3ccf1a850ce25a3e9e28d7b20286fcb3d7
danijeldz <danijeldzankic@hotmail.com>
2020-03-05 21:32:00 +0100

    created router links programmaticaly with use of push and go routing functions.

--- routing/src/components/Navbar.vue
+++ routing/src/components/Navbar.vue
@@ -1,8 +1,22 @@
 <template>
-  <nav>
+  <nav class="main-nav">
     <ul>
       <li><router-link to="/">Home</router-link></li>
       <li><router-link :to="{ name: 'About' }">About</router-link></li>
+    </ul>
+    <h2>User Profiles</h2>
+    <ul>
+      <li v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{ name: 'ViewProfile', params: { user_id: id } }">
+          <span>Profile {{ id }}</span>
+        </router-link>
+      </li>
+    </ul>
+    <h2>Navigation controls</h2>
+    <ul>
+      <li><button @click="goBack">Back</button></li>
+      <li><button @click="goHome">Redirect to Home</button></li>
+      <li><button @click="goForward">Forward</button></li>
     </ul>
   </nav>
 </template>
@@ -11,7 +25,20 @@
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      userIds: [1, 2, 3, 4, 5]
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: "Home" });
+    },
+    goBack() {
+      this.$router.go(-1);
+    },
+    goForward() {
+      this.$router.go(1);
+    }
   }
 };
 </script>
@@ -23,6 +50,7 @@ ul {
 }
 ul > li {
   display: inline-block;
+  margin: 10px;
 }
 a {
   color: #42b983;
